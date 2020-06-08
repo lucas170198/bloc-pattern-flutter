695d66148cd0dbf6d8c0dfd2cc3e90610e7791d0
lucas.amorim <lucas.amorim@nubank.com.br>
2020-06-08 13:46:46 -0300

    Firts sections

---
+++ MovieList/android/app/src/main/kotlin/com/example/MovieList/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.MovieList
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ MovieList/lib/main.dart
@@ -0,0 +1,4 @@
+import 'package:MovieList/src/app.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(App());
